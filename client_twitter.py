d1215eace25aac6d06dd41ae8cfbd75de0aa5eb2
wesley <wesleyumsocaminho@gmail.com>
2022-01-11 16:19:36 -0300

    remove variaveis tokens

--- client_twitter.py
+++ client_twitter.py
@@ -1,11 +1,6 @@
 import tweepy as tw
 import os
 
-# consumer_key = 'EddqPzmNlCmUCQBPf7aLlg7pi'
-# consumer_secret = 'ishRiJYIr99ko8ojtJHtcJyXxsr4Q5le5GmJ1LW0gEGewCujRy'
-# access_token_key = '139177787-oCipscf6pImkxaxxZ9EyT4g1DwckqxsP5jPaJPU7'
-# access_token_secret = 'cmxnZXcnjp6uts78yoP1BaNUuEJUVvbY3ZGZF5efemp4w'
-
 access_token_key = os.environ['access_token_key']
 access_token_secret = os.environ['access_token_secret']
 consumer_key = os.environ['consumer_key']
